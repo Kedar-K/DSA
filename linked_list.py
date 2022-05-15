52e91f8fff0ebc1c3026d55633fb46c6fba349fc
Kedar Kodgire <kedarkodgire.kk@gmail.com>
2022-05-15 22:37:13 +0530

    creating ll

---
+++ linked_list.py
@@ -0,0 +1,43 @@
+class Node:
+    '''
+    used for creating individual nodes of LinkedList
+    '''
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next = None
+
+    def __repr__(self) -> str: # repr is string representation of file
+        return self.data
+
+class LinkedList:
+    '''
+    Used to initialize the LinkedList with head as None
+    '''
+    def __init__(self, nodes = None) -> None:
+        self.head = None
+
+        if nodes is not None:
+            '''
+            creating ll's quickly
+            ll = LinkedList(["1","2","3","4","5"])
+            print(ll)
+            '''
+            node = Node(data=nodes.pop(0))
+            self.head = node
+            for elem in nodes:
+                node.next = Node(data=elem)
+                node = node.next
+
+    def __repr__(self) -> str:
+        node = self.head
+        nodes = []
+        while node is not None:
+            nodes.append(node.data)
+            node = node.next
+        nodes.append('None')
+        return '->'.join(nodes)
+
+ll = LinkedList(["1","2","3","4","5"])
+print(ll)
+
+
